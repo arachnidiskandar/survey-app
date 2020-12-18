31024119c53f11f09f05ab04aedea24f643f1366
Augusto <augstarachrafael@gmail.com>
2020-12-18 15:24:07 -0300

    Survey endpoint validation

--- back-end/src/controllers/survey.ts
+++ back-end/src/controllers/survey.ts
@@ -1,7 +1,9 @@
 import { Request, Response } from 'express';
+import Joi, { array } from 'joi';
 import mongoose from 'mongoose';
 
 import logging from '@config/logging';
+import { QuestionType } from '@interfaces/survey';
 import Survey from '@models/survey';
 
 const NAMESPACE = 'Survey Controller';
@@ -14,18 +16,38 @@ const getAllSurveys = (req: Request, res: Response): void => {
 };
 
 const createSurvey = (req: Request, res: Response) => {
-  const { title, coordinator, questions } = req.body;
+  const questionSchema = Joi.object({
+    title: Joi.string().required(),
+    questionType: Joi.string().valid('MULTIPLE CHOICE', 'MULTIPLE SELECT', 'OPEN TEXT', 'YES OR NO'),
+    choices: Joi.when('questionType', {
+      is: Joi.string().valid('OPEN TEXT', 'YES OR NO'),
+      then: Joi.forbidden(),
+      otherwise: Joi.array().min(2).items(Joi.string()).required(),
+    }),
+  });
+  const surveySchema = Joi.object({
+    title: Joi.string().required(),
+    coordinator: Joi.required(),
+    questions: Joi.array().items(questionSchema),
+    availableToAnyone: Joi.boolean().required(),
+  });
+  const { value, error } = surveySchema.validate(req.body);
+  const { title, coordinator, questions, availableToAnyone } = value;
+  if (error) {
+    return res.status(400).json({ message: error.details[0].message });
+  }
 
   const survey = new Survey({
     _id: new mongoose.Types.ObjectId(),
     title,
     coordinator,
     questions,
+    availableToAnyone,
   });
 
   return survey
     .save()
     .then((result) => res.status(201).json({ survey: result }))
-    .catch((error) => res.status(500).json({ message: error.message, error }));
+    .catch((err) => res.status(500).json({ message: err.message, err }));
 };
 export default { getAllSurveys, createSurvey };
--- back-end/src/interfaces/survey.ts
+++ back-end/src/interfaces/survey.ts
@@ -1,19 +1,21 @@
 import { Document } from 'mongoose';
 
-enum QuestionType {
-  MULTIPLE_CHOICE,
-  SELECT_MULTIPLE_CHOICE,
-  OPEN_TEXT,
-  YES_NO,
+export enum QuestionType {
+  MULTIPLE_CHOICE = 'MULTIPLE CHOICE',
+  SELECT_MULTIPLE = 'SELECT MULTIPLE',
+  OPEN_TEXT = 'OPEN TEXT',
+  YES_NO = 'YES OR NO',
 }
 
 interface IQuestion {
   title: string;
   questionType: QuestionType;
+  choices?: string[];
 }
 
 export default interface ISurvey extends Document {
   title: string;
   coordinator: any;
   questions: IQuestion[];
+  availableToAnyone: boolean;
 }
